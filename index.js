acf8c0f87018509698e8645eeb6a64890300192e
NaimaNITU <toktikproject@gmail.com>
2025-05-23 23:16:56 +0600

    Front end cross origin permission

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // CORS SETUP
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite frontend
+    origin: "https://recipe-book-front-end.vercel.app", // Vite frontend
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
