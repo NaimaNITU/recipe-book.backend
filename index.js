544885a4a449060b1c0db84b343bbe62a174ae43
NaimaNITU <toktikproject@gmail.com>
2025-05-23 22:53:21 +0600

    post request added

--- index.js
+++ index.js
@@ -28,5 +28,26 @@ mongoose
     process.exit(1);
   });
 
+// Create
+app.post("/recipes", async (req, res) => {
+  try {
+    const recipe = await Recipe.create(req.body);
+    res.status(201).json(recipe);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// DEFINE MODEL
+const RecipeSchema = new mongoose.Schema({}, { strict: false });
+const Recipe = mongoose.model("Recipe", RecipeSchema, "recipes");
+
+// ROUTES
+
+// check server ok or not
+app.get("/", (req, res) => {
+  res.send("<h1>🍽️ Recipe Book API</h1><p>Welcome to the API</p>");
+});
+
 // BODY PARSER
 app.use(express.json());
