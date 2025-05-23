154a2046aec0563f457b1a768f0564f8d8cb94e0
NaimaNITU <toktikproject@gmail.com>
2025-05-23 22:41:27 +0600

    monodb connected

--- index.js
+++ index.js
@@ -14,5 +14,19 @@ app.use(
   })
 );
 
+// CONNECT TO MONGODB
+const MONGODB_URI = `${process.env.MONGODB_URI}`;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
 // BODY PARSER
 app.use(express.json());
